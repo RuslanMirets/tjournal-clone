265475711eb57ee0f731ccb52ff39e720044ee21
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-12-08 17:12:43 +0300

    Урок №3

--- data.js
+++ data.js
@@ -4,12 +4,14 @@ module.exports = {
       {
         id: 1,
         user: {
+          id: 1,
           fullname: 'Вася Пупкин',
           avatarUrl:
             'https://leonardo.osnova.io/104b03b4-5173-fd9f-2af9-b458dddc4a23/-/scale_crop/108x108/-/format/webp/',
         },
         text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
         post: {
+          id: 1,
           title: 'Какая у вас дома ванна?',
         },
         createdAt: new Date().toString(),
@@ -17,12 +19,14 @@ module.exports = {
       {
         id: 2,
         user: {
+          id: 1,
           fullname: 'Вася Пупкин',
           avatarUrl:
             'https://leonardo.osnova.io/441ded4b-8e6c-57da-834c-4719dfa852b4/-/scale_crop/64x64/',
         },
         text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
         post: {
+          id: 1,
           title: 'Какая у вас дома ванна?',
         },
         createdAt: new Date().toString(),
@@ -30,12 +34,14 @@ module.exports = {
       {
         id: 3,
         user: {
+          id: 1,
           fullname: 'Вася Пупкин',
           avatarUrl:
             'https://leonardo.osnova.io/17f7221e-2586-8428-6283-66b4d4b11a38/-/scale_crop/64x64/',
         },
         text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
         post: {
+          id: 1,
           title: 'Какая у вас дома ванна?',
         },
         createdAt: new Date().toString(),
@@ -45,12 +51,14 @@ module.exports = {
       {
         id: 1,
         user: {
+          id: 1,
           fullname: 'Вася Пупкин',
           avatarUrl:
             'https://leonardo.osnova.io/104b03b4-5173-fd9f-2af9-b458dddc4a23/-/scale_crop/108x108/-/format/webp/',
         },
         text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
         post: {
+          id: 1,
           title: 'Какая у вас дома ванна?',
         },
         createdAt: new Date().toString(),
